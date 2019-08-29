61309ba22483c876957162c04c0cf7e4025a97e5
Trung Hieu Nguyen <trunghieu.nth@gmail.com>
2019-08-29 15:02:29 +0700

    Add tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,8 @@
 const tailwindcss = require("tailwindcss");
 
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/views/*.vue"],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-});
-
 module.exports = {
   plugins: {
     tailwindcss,
-    autoprefixer: {},
-    purgecss
+    autoprefixer: {}
   }
 };
